0057ed5d0c7e62b2ca0253b66eb2bea3fa52f856
Matthew Wotus <mawotus@gmail.com>
2024-12-29 20:23:00 -0500

    CSS and JavaScript updates

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -6,35 +6,38 @@ defineProps({
 onMounted(() => {
   const dollarAmount = document.getElementById("dollar-amount");
   const resetButton = document.getElementById("reset-button");
+  const dateText = document.getElementById("date");
   const dateContainer = document.getElementById("date-container");
+  const errorContainer = document.getElementById("error");
   const btcAllocation = document.getElementById("btc-allocation");
   const ethAllocation = document.getElementById("eth-allocation");
   const btcRateContainer = document.getElementById("btc-rate-container");
   const ethRateContainer = document.getElementById("eth-rate-container");
+  let fetchCounter = 0;
   let btcRate;
   let ethRate;
   const url = "https://api.coinbase.com/v2/exchange-rates?currency=USD";
   function getRates() {
     fetch(url)
-      .then(async response => {
-        const data = await response.json();
-        if (response.ok) {
-          const rates = data.data["rates"];
-          btcRate = rates.BTC;
-          ethRate = rates.ETH;
-          btcRateContainer.innerText = parseFloat(btcRate).toFixed(6);
-          ethRateContainer.innerText = parseFloat(ethRate).toFixed(6);
-        } else {
-          const error = (data && data.message) || response.statusText;
-          return Promise.reject(error);
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
+        return response.json();
+      })
+      .then(data => {
+        const rates = data.data["rates"];
+        btcRate = rates.BTC;
+        ethRate = rates.ETH;
+        btcRateContainer.innerText = parseFloat(btcRate).toFixed(6);
+        ethRateContainer.innerText = parseFloat(ethRate).toFixed(6);
       })
       .catch(error => {
-        this.errorMessage = error;
+        errorContainer.style.visibility = "visible";
         console.error("There was an error!", error);
       });
   }
-  // The getRates() function fetches the cryptocurrency exchange rates from the given API endpoint, parses the body text of the response as JSON, finds the exchange rates for BTC and ETH, and displays the exchange rates on the webpage. //
+  // The getRates() function fetches the cryptocurrency exchange rates from the given API endpoint, parses the body text of the response as JSON, finds the exchange rates for BTC and ETH, and displays the exchange rates on the webpage. If there is an error fetching the cryptocurrency exchange rates, the function displays an error message on the webpage. //
   getRates();
   function getDate() {
     const date = new Date().toLocaleString();
@@ -42,26 +45,33 @@ onMounted(() => {
   }
   // The getDate() function creates a new date object, converts the object to a string of the date in the local timezone, and displays the date on the webpage. //
   getDate();
-  dollarAmount.addEventListener("input", function () {
-    const dollarAmountValue = dollarAmount.value;
+  dollarAmount.addEventListener("input", function (e) {
+    const dollarAmountValue = e.target.value;
     const btcSplit = dollarAmountValue * 0.7;
     const ethSplit = dollarAmountValue * 0.3;
-    getRates();
     const btcAmount = (btcRate * btcSplit).toFixed(6);
     const ethAmount = (ethRate * ethSplit).toFixed(6);
     btcAllocation.value = btcAmount;
     ethAllocation.value = ethAmount;
   })
-  // The event listener added to the dollar amount variable takes the value of the dollar input field, calculates a 70/30 split, calls the getRates() function, calculates the BTC and ETH equivalent of the respective dollar amounts, rounds those numbers to six decimal places, and displays them on the webpage. The event listener runs whenever the value of the dollar input field is changed. //
+  // The event listener added to the dollar amount variable gets the value of the element where the event occurred (i.e. the dollar input field), calculates a 70/30 split, calculates the BTC and ETH equivalent of the respective dollar amounts, rounds those numbers to six decimal places, and displays them on the webpage. The event listener runs whenever the value of the dollar input field is changed. //
   resetButton.addEventListener("click", function () {
     dollarAmount.value = "";
     btcAllocation.value = "";
     ethAllocation.value = "";
-    getRates();
-    getDate();
-    dollarAmount.focus();
+    fetchCounter = fetchCounter + 1;
+    if (fetchCounter > 5) {
+      alert("You've hit the maximum number of conversions allowed in a 10 minute period. We have to limit the number of conversions to ensure everyone is able to use the calculator. Please refresh the page in 10 minutes to continue using the calculator.");
+      dollarAmount.setAttribute("readonly", "");
+      resetButton.setAttribute("disabled", "");
+      dateText.innerText = "";
+    } else {
+      getRates();
+      getDate();
+      dollarAmount.focus();
+    }
   })
-  // The event listener added to the reset button clears the values of the dollar input, the BTC allocation input, and the ETH allocation input. It also calls the getRates() and getDate() functions and sets the focus of the webpage to the dollar input. The event listener runs whenever the reset button is clicked. //
+  // The event listener added to the reset button clears the values of the dollar input, the BTC allocation input, and the ETH allocation input. If the reset button has been clicked more than five times, it displays an alert with a message, sets the dollar input field to a read only input field, disables the reset button, and clears the inner text of the div containing the date. If the reset button has been clicked five times or fewer, the function calls the getRates() and getDate() functions and sets the focus of the webpage to the dollar input. The event listener runs whenever the reset button is clicked. //
 });
 </script>
 
@@ -72,13 +82,14 @@ onMounted(() => {
     <div class="inner-container dollars">
       <div class="dollar-input">
         <label for="dollar-amount">Investable Assets (USD)</label>
-        <input type="number" min="0" step="0.01" id="dollar-amount" name="dollar-amount" autofocus="autofocus"
+        <input type="number" min="0" id="dollar-amount" name="dollar-amount" autofocus="autofocus"
           onkeydown="return event.keyCode !== 69 && event.keyCode !== 187 && event.keyCode !== 189">
       </div>
       <div id="reset">
         <button id="reset-button">Reset</button>
       </div>
-      <div id="date">Conversion rates are as of <span id="date-container"></span></div>
+      <div id="date">Exchange rates are as of <span id="date-container"></span></div>
+      <div id="error">There was an error getting the exchange rates. Please try again later.</div>
     </div>
     <div class="inner-container crypto">
       <div class="btc">
